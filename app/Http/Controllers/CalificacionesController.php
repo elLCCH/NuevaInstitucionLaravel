a707e6a8251caacfc85e5d149a3ea2b94fcbb520
Luis <cristian.98oruro@gmail.com>
2024-11-16 11:32:57 -0400

    nueva superconsulta

--- app/Http/Controllers/CalificacionesController.php
+++ app/Http/Controllers/CalificacionesController.php
@@ -447,6 +447,7 @@ class CalificacionesController extends Controller
         $dataRequest = $request->all();
         $idCurso= $dataRequest['curso_id'];
         $anioid= $dataRequest['anio_id'];
+        $mallatxt= $dataRequest['Malla'];
         // OBTENER DATOS DE CURSO MEDIANTE ID
         $cursoData= DB::select("SELECT * from cursos where id='$idCurso'");
         // $curso = 'PRIMERO SUPERIOR A';
@@ -483,7 +484,7 @@ class CalificacionesController extends Controller
         $materias=$materiasEncontradas;
 
         //OBTENER INFO DE ESTUDIANTE DE SUS CALIFICACIONES DE TODAS SUS MATERIAS
-        $dataCentralizador = DB::select("CALL getCentralizadorFinalLCCHFin('$materias',$request->estudiante_id,'$curso',$anioid)"); // PARA LLAMAR PROCEDURES
+        $dataCentralizador = DB::select("CALL getCentralizadorFinalLCCHFinish('$materias',$request->estudiante_id,'$curso',$anioid,'$mallatxt')"); // PARA LLAMAR PROCEDURES
 
         //VERIFICAR CALIFICACIONES
         $contadorReprobados=0;
@@ -612,10 +613,11 @@ class CalificacionesController extends Controller
     $TipoEsts = $request->input('Arrastre');
     // $TipoEsts = 'ARRA1STREss';
     $curso = $request->input('NivelCurso');
+    $mallatxt = $request->input('Malla');
     $id_gestion= $request->input('Anio_id');
     //CONSEGUIR ID DE CURSO POR NIVEL DE CURSO
     // $Cursodata= DB::select("SELECT *,SUBSTRING(Sigla,5,7) as SiglaNum from cursos where NivelCurso='$curso' and anio_id=$id_gestion order by SiglaNum asc"); //ANTES POR LOS NUMEROS
-    $Cursodata= DB::select("SELECT * from cursos where NivelCurso='$curso' and anio_id=$id_gestion order by Rango asc"); //AHORA ORDENAMOS POR RANGO
+    $Cursodata= DB::select("SELECT * from cursos where NivelCurso='$curso' and Malla='$mallatxt' and anio_id=$id_gestion order by Rango asc"); //AHORA ORDENAMOS POR RANGO
     //GUARDANDO ID DEL PRIMER CURSO DE LA PRIMERA FILA
     $idCurso = $Cursodata[0]->id;
 
@@ -784,14 +786,14 @@ class CalificacionesController extends Controller
     $data = array(); //ES CON SEXO
     foreach ($dataEstsNew as $est) {
         // $CentralizadorData = Estudiantes::where('id','=', $C->estudiante_id)->first();
-        $CentralizadorData=DB::select("CALL getCentralizadorFinalLCCHFin('$materias',$est->estudiante_id,'$curso',$id_gestion)"); // PARA LLAMAR PROCEDURES
+        $CentralizadorData=DB::select("CALL getCentralizadorFinalLCCHFinish('$materias',$est->estudiante_id,'$curso',$id_gestion,'$mallatxt')"); // PARA LLAMAR PROCEDURES
         // $CentralizadorData=DB::select("CALL getCentralizadorFinal(?,?,?,?), ['$materias',$est->estudiante_id,'$curso',$id_gestion,$materias]"); // PARA LLAMAR PROCEDURES
         $data[] = $CentralizadorData[0];
     }
     $data2 = array(); //ES SIN SEXO
     foreach ($dataEstsNew as $est) {
         // $CentralizadorData = Estudiantes::where('id','=', $C->estudiante_id)->first();
-        $CentralizadorData=DB::select("CALL getCentralizadorFinalLCCHFinSinSexo('$materias',$est->estudiante_id,'$curso',$id_gestion)"); // PARA LLAMAR PROCEDURES
+        $CentralizadorData=DB::select("CALL getCentralizadorFinalLCCHFinishSinSexo('$materias',$est->estudiante_id,'$curso',$id_gestion,'$mallatxt')"); // PARA LLAMAR PROCEDURES
         // $CentralizadorData=DB::select("CALL getCentralizadorFinalSinSexo(?,?,?,?), ['$materias',$est->estudiante_id,'$curso',$id_gestion,$materias]"); // PARA LLAMAR PROCEDURES
         $data2[] = $CentralizadorData[0];
     }
@@ -955,6 +957,54 @@ class CalificacionesController extends Controller
 // END;
 
 //     $$
+
+
+
+
+
+
+//------------NEW NOVIEMBRE 2024. CODIGO TOMANDO EN CUENTA LA MALLA PARA EVITAR PROBLEMAS///data: aumentar el "SinSexo" al final del nombre y quitar el 'estudiantes.Sexo,'
+// -- drop PROCEDURE getCentralizadorFinalLCCHFinish;
+// DELIMITER ;
+// DELIMITER $$
+// CREATE OR REPLACE PROCEDURE getCentralizadorFinalLCCHFinish(IN arreglo TEXT,IN idEst INT, IN nombreCurso VARCHAR(255),IN idGestion INT,IN Mallatxt VARCHAR(255))
+// BEGIN
+
+//   DECLARE _result varchar(10000) DEFAULT '';
+//   -- DECLARE _value varchar(50);
+// DECLARE i INT DEFAULT 0;
+// DECLARE elemento VARCHAR(255);
+// DECLARE numElementos INT;
+// SET numElementos = JSON_LENGTH(arreglo);
+
+
+// SET @ini = 'SELECT estudiantes.Ap_Paterno, estudiantes.Ap_Materno, estudiantes.Nombre, estudiantes.CI,estudiantes.id,estudiantes.Especialidad,estudiantes.Sexo,';
+// SET @fin = CONCAT('FROM estudiantes LEFT JOIN calificaciones ON estudiantes.id = calificaciones.estudiante_id LEFT JOIN cursos ON calificaciones.curso_id = cursos.id where cursos.NivelCurso="',nombreCurso,'" AND cursos.Malla="',Mallatxt,'" AND estudiantes.id="',idEst,'" AND calificaciones.anio_id="',idGestion,'" ORDER BY estudiantes.Ap_Paterno,estudiantes.Ap_Materno,estudiantes.Nombre, estudiante_id, cursos.Sigla;');
+
+// WHILE i < numElementos-1 DO
+//     SET elemento = JSON_EXTRACT(arreglo, CONCAT('$[', i, ']'));
+//     -- realizar alguna operación con cada elemento del arreglo
+//     SET _result = CONCAT(_result,'SUM(CASE WHEN cursos.NombreCurso=',elemento,' AND estudiantes.id = ',idEst,' AND calificaciones.anio_id = ',idGestion,'  THEN
+//                           (IF (calificaciones.PruebaRecuperacion IS NULL,calificaciones.Promedio ,calificaciones.PruebaRecuperacion ))
+//                          ELSE NULL END) as ',elemento,',');
+
+//     -- SELECT elemento;
+//     SET i = i + 1;
+// END WHILE;
+
+// -- ULTIMA ITERACION PARA EVITAR ERROR DEL FROM
+//     SET elemento = JSON_EXTRACT(arreglo, CONCAT('$[', i, ']'));
+//     -- realizar alguna operación con cada elemento del arreglo
+//     SET _result = CONCAT(_result,'SUM(CASE WHEN cursos.NombreCurso=',elemento,' AND estudiantes.id = ',idEst,' AND calificaciones.anio_id = ',idGestion,'  THEN
+//                          (IF (calificaciones.PruebaRecuperacion IS NULL,calificaciones.Promedio ,calificaciones.PruebaRecuperacion ))
+//                          ELSE NULL END) as ',elemento,' ');
+//     SET @sql = CONCAT(@ini,_result,@fin);
+//   PREPARE stmt FROM @sql;
+//   EXECUTE stmt;
+//   DEALLOCATE PREPARE stmt;
+// END;
+
+// $$
     }
     public function EncontrarNivelCurso(Request $request,$id)
     {
